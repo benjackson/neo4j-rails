3cb492480dd59664b76f7c24d22551b0c0ed6fdd
Ben Jackson <ben@localamerica.com>
2010-10-11 11:55:54 -0400

    Separated out distinct functions and created a way to share the delayed save between nodes and relationships

--- lib/neo4j/delayed_save.rb
+++ lib/neo4j/delayed_save.rb
@@ -1,69 +1,110 @@
 # Extension to Neo4j::NodeMixin to delay creating/updating properties
 # until #save is called.
-module Neo4j::DelayedSave
-  def id
-    persisted? ? neo_id : nil
-  end
-
-  # Override NodeMixin#init_without_node to save the properties for
-  # when #save is called.
-  def init_without_node(props) # :nodoc:
-    raise "Can't use Neo4j::Model with anonymous classes" if self.class.name == ""
-    props[:_classname] = self.class.name
-    @_unsaved_props = props || {}
-    @persisted = false
-  end
-
-  # Ensure nodes loaded from the database are marked as persisted.
-  def init_with_node(node)
-    super
-    @persisted = true
-    @_unsaved_props = {}
-  end
-
-  # Delegate property access to temporary properties first
-  def [](key)
-    if @_unsaved_props.has_key?(key)
-      @_unsaved_props[key]
-    elsif @_java_node
-      @_java_node[key]
+module Neo4j
+  module DelayedSave
+    extend ActiveSupport::Concern
+    
+    class RecordInvalidError < RuntimeError
+      attr_reader :record
+      def initialize(record)
+        @record = record
+        super(@record.errors.full_messages.join(", "))
+      end
+    end
+      
+    def persisted?
+      @persisted
+    end
+      
+    def props
+      # allow access to props when the java object isn't there
+      persisted? ? super : @_unsaved_props
+    end
+      
+    def init_props(props)
+      @_unsaved_props = props || {}
+      @persisted = false
+    end
+  
+    # Delegate property access to temporary properties first
+    def [](key)
+      if @_unsaved_props.has_key?(key)
+        @_unsaved_props[key]
+      elsif @_java_node
+        @_java_node[key]
+      end
+    end
+  
+    # Delegate property write to temporary properties
+    def []=(key, value)
+      @_unsaved_props[key] = value
+    end
+  
+    def reload(*)
+      @_unsaved_props.clear
+      self
+    end
+    alias_method :reset, :reload
+  
+    def save(*)
+      create_or_update
+    end
+    
+    def save!
+      unless save
+        raise RecordInvalidError.new(self)
+      end
+    end
+    
+    def del
+      super if persisted?
+    end
+    
+    alias_method :destroy, :del
+    
+    def id
+      persisted? ? neo_id : nil
+    end
+    
+    module ClassMethods
+      def primary_key
+        'id'
+      end
+      
+      def create(*args)
+        new(*args).tap {|o| o.save }
+      end
+      
+      def create!(*args)
+        new(*args).tap {|o| o.save! }
+      end
+      
+      def inherited(subc) # :nodoc:
+        # Make subclasses of each have their own root class/indexer
+        subc.instance_eval do
+          def root_class
+            self
+          end
+        end
+      end
+    end
+    
+    private
+    def create_or_update
+      result = persisted? ? update : create
+      result != false
+    end
+    
+    def update
+      # call the java property mixin's update method
+      super(@_unsaved_props)
+      @_unsaved_props.clear
+      true
+    end
+    
+    def create
+      @_unsaved_props.clear
+      @persisted = true
     end
   end
-
-  # Delegate property write to temporary properties
-  def []=(key, value)
-    @_unsaved_props[key] = value
-  end
-
-  def reload(*)
-    @_unsaved_props.clear
-    self
-  end
-  alias_method :reset, :reload
-
-  def save(*)
-    create_or_update
-  end
-  
-  private
-  def create_or_update
-    result = persisted? ? update : create
-    result != false
-  end
-  
-  def create
-    @_java_node = Neo4j.create_node
-    @_java_node._wrapper = self
-    @_unsaved_props.each {|k,v| @_java_node[k] = v }
-    @_unsaved_props.clear
-    update_index  # TODO: Move indexation to its own module
-    Neo4j.event_handler.node_created(self)
-    @persisted = true
-  end
-  
-  def update
-    @_unsaved_props.each {|k,v| @_java_node[k] = v }
-    @_unsaved_props.clear
-  end
-    
 end
--- lib/neo4j/model.rb
+++ lib/neo4j/model.rb
@@ -1,79 +1,39 @@
 require 'neo4j'
 require 'neo4j/extensions/reindexer'
 require 'active_model'
+require 'neo4j/attributes'
 require 'neo4j/delayed_save'
+require 'neo4j/node_creation'
 require 'neo4j/callbacks'
 require 'neo4j/validations'
 
 module Neo4j
   class Model
     include NodeMixin
+    include Attributes
     include DelayedSave
+    include NodeCreation
     include Validations
     include Callbacks
     include ActiveModel::Conversion
     
-    class RecordInvalidError < RuntimeError
-      attr_reader :record
-      def initialize(record)
-        @record = record
-        super(@record.errors.full_messages.join(", "))
-      end
+    # Override NodeMixin#init_without_node to save the properties for
+    # when #save is called.
+    def init_without_node(props) # :nodoc:
+      raise "Can't use Neo4j::Model with anonymous classes" if self.class.name == ""
+      init_props(props)
+    end
+  
+    # Ensure nodes loaded from the database are marked as persisted.
+    def init_with_node_with_persistence(java_node)
+      init_with_node_without_persistence(java_node)
+      @persisted = true
+      @_unsaved_props = {}
     end
     
-    def persisted?
-      @persisted
-    end
-  
-    def read_attribute_for_validation(key)
-      self[key]
-    end
-  
-    def attributes=(attrs)
-      attrs.each do |k,v|
-        unless k == "_neo_id"           # ignore the _neo_id when setting attributes from a bunch of attrs from another model
-          if respond_to?("#{k}=")
-            send("#{k}=", v)
-          else
-            self[k] = v
-          end
-        end
-      end
-    end
-    
-    def props
-      # allow access to props when the java object isn't there
-      persisted? ? super : @_unsaved_props
-    end
-    
-    # return the props without the internal vars
-    def attributes
-      props.reject { |k, v| k == :_classname || k == :_neo_id }
-    end
-    
-    def update_attributes(attributes)
-      self.attributes = attributes
-      save
-    end
-  
-    def update_attributes!(attributes)
-      self.attributes = attributes
-      save!
-    end
-  
-    alias_method :destroy, :del
-  
-    def save!
-      unless save
-        raise RecordInvalidError.new(self)
-      end
-    end
+    alias_method_chain :init_with_node, :persistence
   
     class << self # class methods
-      def primary_key
-        'id'
-      end
-    
       def load(*ids)
         result = ids.map {|id| Neo4j.load_node(id) }
         if ids.length == 1
@@ -97,23 +57,6 @@ module Neo4j
           load(*args)
         else
           super
-        end
-      end
-    
-      def create(*args)
-        new(*args).tap {|model| model.save }
-      end
-      
-      def create!(*args)
-        new(*args).tap {|model| model.save! }
-      end
-    
-      def inherited(subc) # :nodoc:
-        # Make subclasses of Neo4j::Model each have their own root class/indexer
-        subc.instance_eval do
-          def root_class
-            self
-          end
         end
       end
     end
