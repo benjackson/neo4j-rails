84b134a22848659bbd45537130a8f431a29db2e6
Ben Jackson <ben@localamerica.com>
2010-10-11 11:46:34 -0400

    Separated out the callback functions

--- lib/neo4j/callbacks.rb
+++ lib/neo4j/callbacks.rb
@@ -3,26 +3,30 @@ module Neo4j
     extend ActiveSupport::Concern
     
     included do
+      [:create_or_update, :create, :update, :destroy].each do |method|
+        alias_method_chain method, :callbacks
+      end
+      
       extend ActiveModel::Callbacks
       
       define_model_callbacks :create, :save, :update, :destroy
     end
     
-    def destroy #:nodoc:
-      _run_destroy_callbacks { super }
+    def destroy_with_callbacks #:nodoc:
+      _run_destroy_callbacks { destroy_without_callbacks }
     end
     
     private
-    def create_or_update #:nodoc:
-      _run_save_callbacks { super }
+    def create_or_update_with_callbacks #:nodoc:
+      _run_save_callbacks { create_or_update_without_callbacks }
     end
 
-    def create #:nodoc:
-      _run_create_callbacks { super }
+    def create_with_callbacks #:nodoc:
+      _run_create_callbacks { create_without_callbacks }
     end
 
-    def update(*) #:nodoc:
-      _run_update_callbacks { super }
+    def update_with_callbacks(*) #:nodoc:
+      _run_update_callbacks { update_without_callbacks }
     end
   end
 end
