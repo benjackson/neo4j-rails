5a9da144c3aba0062b91dac46530a3250ee2038f
Ben Jackson <ben@localamerica.com>
2010-10-11 13:00:09 -0400

    Changed so it works with normal neo nodes

--- lib/neo4j/persistance_validator.rb
+++ lib/neo4j/persistance_validator.rb
@@ -1,5 +1,5 @@
 class PersistedValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors.add(attribute, "must be saved beforehand") unless value.respond_to?(:persisted?) && value.persisted?
+    record.errors.add(attribute, "must be saved beforehand") if value.respond_to?(:persisted?) && !value.persisted?
   end
 end
