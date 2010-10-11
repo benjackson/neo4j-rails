194981e8e76b7b823570fb00145ebc96860112f1
Ben Jackson <ben@localamerica.com>
2010-10-11 11:51:43 -0400

    Separated out the validations

--- lib/neo4j/validations.rb
+++ lib/neo4j/validations.rb
@@ -3,25 +3,27 @@ module Neo4j
     extend ActiveSupport::Concern
     include ActiveModel::Validations
     
-    def save
-      valid?(:save) ? super : false
+    included do
+      [:create_or_update, :create, :update].each do |method|
+        alias_method_chain method, :validation
+      end
     end
     
-    def destroy
-      valid?(:destroy) ? super : false
+    def read_attribute_for_validation(key)
+      respond_to?(key) ? send(key) : self[key]
     end
     
     private
-      def create_or_update #:nodoc:
-        valid?(:save) ? super : false
+      def create_or_update_with_validation #:nodoc:
+        valid?(:save) ? create_or_update_without_validation : false
       end
   
-      def create #:nodoc:
-        valid?(:create) ? super : false
+      def create_with_validation #:nodoc:
+        valid?(:create) ? create_without_validation : false
       end
   
-      def update(*) #:nodoc:
-        valid?(:update) ? super : false
+      def update_with_validation(*) #:nodoc:
+        valid?(:update) ? update_without_validation : false
       end
   end
 end
