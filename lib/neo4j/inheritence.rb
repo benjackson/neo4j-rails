28e9626f00678a1ae64820ef05f7e7dc2f06d3de
Ben Jackson <ben.jackson1@gmail.com>
2010-10-17 19:13:49 +0100

    Seperated out the things that need to get done after inheritence

---
+++ lib/neo4j/inheritence.rb
@@ -0,0 +1,23 @@
+module Neo4j
+  module Inheritence
+    extend ActiveSupport::Concern
+    
+    module ClassMethods
+      def inherited(subclass) # :nodoc:
+        # Make subclasses of each have their own root class/indexer
+        subclass.instance_eval do
+          def root_class
+            self
+          end
+        end
+        
+        # index the same properties as this class
+        indexer.property_indexer.properties.each do |property|
+          subclass.indexer.add_index_on_property(property)
+        end
+        
+        # TODO: index the same relationship properties as this class
+      end
+    end
+  end
+end
